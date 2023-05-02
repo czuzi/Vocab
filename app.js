48adab9368353c11d676f8de9a9cd7c5ef47835e
czuzi <czuzi@czuzi.hu>
2023-05-02 23:56:46 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("Initialize express")
+})
+
+app.listen(3000)
